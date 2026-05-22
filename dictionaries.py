f909b30801878719d63569a4827eebe885821e97
Popangelov <mfaka7342@gmail.com>
2026-05-22 11:55:32 +0300

    disctionaries

---
+++ dictionaries.py
@@ -0,0 +1,14 @@
+# Създаване
+my_dict = {}                                     # празен
+my_dict = {"fruit": "apple", "vegetable": "cucumber"}
+
+# чрез dict()
+dict_arguments = dict(name="George", age=22)
+# {"name": "George", "age": 22}
+
+# много редове
+my_dict = {
+    "fruit": "apple",
+    "vegetable": "cucumber",
+    "diary": "milk",
+}
