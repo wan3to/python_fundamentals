e8069bc47609dd23af9623555156ad9de41bca76
Popangelov <mfaka7342@gmail.com>
2026-05-22 11:56:39 +0300

    functions

---
+++ functions.py
@@ -0,0 +1,12 @@
+# Декларация и извикване
+def print_text(text):
+    print(text)
+
+print_text("Hello")
+
+# без параметри
+def multiply_numbers():
+    result = 5 * 5
+    print(result)
+
+multiply_numbers()
