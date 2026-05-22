a9e285aa6c7d399a2972a7c549586f0403cd69af
Popangelov <mfaka7342@gmail.com>
2026-05-22 11:53:30 +0300

    checking

---
+++ check.py
@@ -0,0 +1,7 @@
+# Проверка за наличие
+my_dict = {"name": "Peter", "age": 22}
+if "name" in my_dict:
+    print(my_dict["name"])
+
+if 22 in my_dict.values():
+    print("22 is a value in the dictionary")
