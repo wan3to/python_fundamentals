42ec31246018fe7a971b36e0e8cf017353e9a800
Popangelov <mfaka7342@gmail.com>
2026-05-22 11:59:14 +0300

    lists

---
+++ lists.py
@@ -0,0 +1,11 @@
+# Дефиниране
+nums = [1, 2, 3]
+empty_list = []
+mixed = [7, "Peter", 9.99]
+
+# Създаване от стринг
+some_text = "a b c d"
+my_list = some_text.split(" ")       # ['a', 'b', 'c', 'd']
+
+# join обратно към стринг
+print("-".join(["a", "b", "c"]))     # "a-b-c"
